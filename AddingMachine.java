fe2bb28293f528837ab8ba300d300aefb085d8d6
Alophox <bluebolt1@hotmail.com>
2020-10-05 11:08:22 -0700

    Initial Upload

---
+++ AddingMachine.java
@@ -0,0 +1,46 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total; //holds the total
+	/**
+	 * Constructor
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * TODO: return total
+	 * @return total(currently returns 0)
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * TODO: implement
+	 * @param value, to be added
+	 */
+	public void add (int value) {
+	}
+	/**
+	 * TODO: implement
+	 * @param value, to be subtracted
+	 */
+	public void subtract (int value) {
+	}
+	/**
+	 * TODO: implement
+	 * @return all operations put into a string
+	 * ie. add(2); sub(4);
+	 * toString() will return
+	 * 0 + 2 - 4
+	 */
+	public String toString () {
+		return "";
+	}
+	/**
+	 * TODO:implement
+	 * clears operations
+	 */
+	public void clear() {
+	}
+}
